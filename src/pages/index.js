4c0ccd201f0e0e564953e21804781b70a36c3c3b
ahmedhosny <i.ahmedhosny@gmail.com>
2023-05-02 20:41:15 -0700

    tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import * as React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return  <h1 className="text-4xl font-bold underline">
+    Hello world!!
+  </h1>
 }
