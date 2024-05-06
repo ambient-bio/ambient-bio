3af383f0924ef87616f92ef9b7867585d3f90016
ahmedhosny <i.ahmedhosny@gmail.com>
2024-05-06 16:03:31 -0700

    margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,8 @@ export default function Home() {
 		<div className="flex min-h-screen max-w-full h-screen grid-cols-1 bg-verylightblue">
 			<GradientCanvas id="canvas"/>
 			<div className="absolute h-full w-full backdrop-blur-lg bg-white/10" />
-			<AmbientLogo className="absolute w-[70px] h-[25px] md:w-[100px] md:h-[30px] mt-8 ml-8 opacity-100" />
-			<SquircleLogo className="absolute w-[15px] h-[15px] md:w-[25px] md:h-[25px] bottom-0 mb-8 ml-8 opacity-100" />
+			<AmbientLogo className="absolute w-[70px] h-[25px] md:w-[100px] md:h-[30px] mt-4 ml-4  md:mt-8 md:ml-8 opacity-100" />
+			<SquircleLogo className="absolute w-[15px] h-[15px] md:w-[25px] md:h-[25px] bottom-0 mb-4 ml-4 md:mb-8 md:ml-8 opacity-100" />
 			<div className="w-3/4 lg:w-1/2 mx-auto text-center text-verylightblue z-10">
 				<div className="text-4xl md:text-5xl lg:text-6xl font-[600] mt-[150px] md:mt-[250px] "> 
 					Transforming cancer care with artificial intelligence.
